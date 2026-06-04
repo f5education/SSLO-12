be99316ec501ee0d77e4f96044d657cbb52100fb
djlamey <76072289+djlamey@users.noreply.github.com>
2026-06-04 16:12:06 -0400

    Update 20-SSLO-12-100-SETUP.sh

--- 20-SSLO-12-100-SETUP.sh
+++ 20-SSLO-12-100-SETUP.sh
@@ -2,15 +2,15 @@
 for i in {1..12}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
 
 # copy archive files from GitHub to bigip1
-curl --silent https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/sslo12-base-v12-beta.scf --output /tmp/sslo12-base-v12-beta.scf
+curl --silent https://raw.githubusercontent.com/f5education/$COURSE_ID/main/sslo12-base-v12-beta.scf --output /tmp/sslo12-base-v12-beta.scf
 sudo scp /tmp/*.scf 192.168.1.31:/var/local/scf
 
-# copy BIG-IP setup script from Github to BIG-IP-01
-curl --silent https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/sslo12_bigip_setup_1.0.sh --output /tmp/sslo12_bigip_setup_1.0.sh
+# copy BIG-IP setup script from Github to bigip1
+curl --silent https://raw.githubusercontent.com/f5education/$COURSE_ID/main/sslo12_bigip_setup_1.0.sh --output /tmp/sslo12_bigip_setup_1.0.sh
 sudo scp /tmp/sslo12_bigip_setup_1.0.sh 192.168.1.31:/shared/tmp
 
 # copy archive files from GitHub to bigip1
-curl --silent https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/sslo1_5_services.ucs --output /tmp/sslo1_5_services.ucs
+curl --silent https://raw.githubusercontent.com/f5education/$COURSE_ID/main/sslo1_5_services.ucs --output /tmp/sslo1_5_services.ucs
 sudo scp /tmp/sslo1_5_services.ucs 192.168.1.31:/var/local/ucs
 sudo ssh 192.168.1.31 tmsh load sys ucs sslo1_5_services.ucs no-license
 sleep 15
