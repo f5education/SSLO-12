54b20995c1a6774c102b0b6ee2f800974e9079bc
djlamey <76072289+djlamey@users.noreply.github.com>
2026-06-25 15:17:48 -0400

    Update 20-SSLO-12-200-AI.sh
    changed ucs file target

--- 20-SSLO-12-200-AI.sh
+++ 20-SSLO-12-200-AI.sh
@@ -1,7 +1,7 @@
 # confirm bigip1 is active
 for i in {1..12}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
 
-UCS=sslo1_5_services.ucs
+UCS=sslo1_ai.ucs
 
 # copy archive files from GitHub to bigip1
 curl --silent https://raw.githubusercontent.com/f5education/$COURSE_ID/main/$UCS --output /tmp/$UCS
