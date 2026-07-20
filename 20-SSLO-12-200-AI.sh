b9a439396cebd8b0b1e3ee4711fa05edbb6bcc43
djlamey <76072289+djlamey@users.noreply.github.com>
2026-07-20 15:34:11 -0400

    Update 20-SSLO-12-200-AI.sh

--- 20-SSLO-12-200-AI.sh
+++ 20-SSLO-12-200-AI.sh
@@ -2,19 +2,41 @@
 for i in {1..12}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
 
 UCS=sslo1_ai.ucs
+SCF=swg_profile.scf
+CSV=local_user_db.csv
 
-# copy archive files from GitHub to bigip1
+# copy files from GitHub
 curl --silent https://raw.githubusercontent.com/f5education/$COURSE_ID/main/$UCS --output /tmp/$UCS
+curl --silent https://raw.githubusercontent.com/f5education/$COURSE_ID/main/$SCF --output /tmp/$SCF
+curl --silent https://raw.githubusercontent.com/f5education/$COURSE_ID/main/$CSV --output /home/student/Downloads/$CSV
+
+# copy files to bigip1
 sudo scp /tmp/$UCS 192.168.1.31:/var/local/ucs
 
 # Load UCS into BIGIP01
 sudo ssh 192.168.1.31 tmsh load sys ucs $UCS no-license
 sleep 15
 
-### WE MAY NEED TO SAVE THIS FOR THE SECOND OF TWO LABS (TMSH)
-# load/merge archive to bigip1 and pause
-#sudo ssh 192.168.1.31 tmsh load sys config merge file sslo12-base-v12-beta.scf
-#sleep 15
-
 # confirm bigip1 is active
 for i in {1..12}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
+
+# provision APM
+sudo ssh 192.168.1.31 tmsh modify sys provision apm level minimum
+sleep 15
+
+# confirm sslo1 is active
+for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
+
+# merge in swg_profile configs
+sudo ssh 192.168.1.31 tmsh load sys config merge file $scf
+
+# confirm sslo1 is active
+for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
+
+# create SWG_Profile
+sudo ssh 192.168.1.31 tmsh modify /apm profile access SWG_Profile generation-action increment
+sudo ssh 192.168.1.31 tmsh save sys config
+#sudo ssh 192.168.1.31 reboot
+
+# confirm bigip1 is active
+for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
