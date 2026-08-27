6ee5e6387b08a9a5a4ab813feb7334c733d19519
djlamey <76072289+djlamey@users.noreply.github.com>
2026-08-27 16:07:38 -0400

    Update 20-SSLO-12-150-POLICY.sh
    corrected code to properly download and load scf file into sslo1

--- 20-SSLO-12-150-POLICY.sh
+++ 20-SSLO-12-150-POLICY.sh
@@ -1,11 +1,11 @@
-# confirm bigip1 is active
-for i in {1..12}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
-
 UCS=sslo1_policy.ucs
+SCF=swg_profile.scf
 
 # copy archive files from GitHub to bigip1
 curl --silent https://raw.githubusercontent.com/f5education/$COURSE_ID/main/$UCS --output /tmp/$UCS
 sudo scp /tmp/$UCS 192.168.1.31:/var/local/ucs
+curl --silent https://raw.githubusercontent.com/f5education/$COURSE_ID/main/$SCF --output /tmp/$SCF
+sudo scp /tmp/$SCF 192.168.1.31:/var/local/scf
 
 # Load UCS into BIGIP01
 sudo ssh 192.168.1.31 tmsh load sys ucs $UCS no-license
@@ -20,7 +20,7 @@ sudo ssh 192.168.1.31 tmsh modify sys provision apm level minimum
 for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
 
 # merge in swg_profile configs
-sudo ssh 192.168.1.31 tmsh load sys config merge file $scf
+sudo ssh 192.168.1.31 tmsh load sys config merge file $SCF
 
 # confirm sslo1 is active
 for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
@@ -29,11 +29,6 @@ for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Ac
 sudo ssh 192.168.1.31 tmsh modify /apm profile access SWG_Profile generation-action increment
 sudo ssh 192.168.1.31 tmsh save sys config
 #sudo ssh 192.168.1.31 reboot
-
-### WE MAY NEED TO SAVE THIS FOR THE SECOND OF TWO LABS (TMSH)
-# load/merge archive to bigip1 and pause
-#sudo ssh 192.168.1.31 tmsh load sys config merge file sslo12-base-v12-beta.scf
-#sleep 15
 
 # confirm bigip1 is active
 for i in {1..12}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
